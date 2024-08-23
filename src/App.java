ac4d39b28768ee020442132b74632cdb516d210e
diedona <diedona@gmail.com>
2024-08-23 10:52:41 -0300

    modulo inicial de leitura de string e int

--- src/App.java
+++ src/App.java
@@ -1,15 +1,29 @@
-import modulos.FirstModule; // importa o package que está no FirstModule.java
+import java.util.Scanner;
 
 public class App {
 
-    // trazendo as outras classes ("modulos")
-    private static FirstModule _firstModule = new FirstModule();
-
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Isso aqui vem do App.java!");
-        _firstModule.ExecuteModule();
-        
+        System.out.println("====================================");
+        System.out.println("     Isso aqui vem do App.java!");
+        System.out.println("====================================");
+
+        String nomeAluno; // para guardar o nome
+        int idadeAluno; // para guardar idade
+        Scanner leitorDeConsole = new Scanner(System.in); // para ler dados digitados no console
+
+        try {
+            System.out.println("Qual o nome do aluno?");
+            nomeAluno = leitorDeConsole.nextLine();   
+            
+            System.out.println("Qual a idade do aluno?");
+            idadeAluno = leitorDeConsole.nextInt();
+        } finally {
+            leitorDeConsole.close();
+        }
+
+        System.out.printf("O aluno se chama: %s\n", nomeAluno);
+        System.out.printf("O aluno tem %d anos\n", idadeAluno);        
     }
 
 }
